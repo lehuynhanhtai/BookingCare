940dbadbcb04b05be4949f0a98e950440775ba6c
Tai <lehuynhanhtai@gmail.com>
2022-09-15 15:17:12 +0700

    firstcomit

---
+++ nodejs/src/migrations/migration-create-booking.js
@@ -0,0 +1,37 @@
+'use strict';
+module.exports = {
+    up: async (queryInterface, Sequelize) =>
+    {
+        await queryInterface.createTable('bookings', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            statusId: {
+                type: Sequelize.STRING
+            },
+            docterId: {
+                type: Sequelize.INTEGER
+            },
+            patientId: {
+                type: Sequelize.INTEGER
+            },
+            date: {
+                type: Sequelize.DATE
+            },
+            timeType: {
+                type: Sequelize.STRING
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: async (queryInterface, Sequelize) =>
+    {
+        await queryInterface.dropTable('bookings');
+    }
+};
