42d73e3c02784cb26e216db0c1a95b27dadc2bac
Tai <lehuynhanhtai@gmail.com>
2022-12-01 09:05:33 +0700

    video 86

--- nodejs/src/services/patientServices.js
+++ nodejs/src/services/patientServices.js
@@ -1,5 +1,6 @@
 import db from "../models/index";
 require('dotenv').config();
+import emailService from './emailService'
 
 let postBookAppointment = (data) =>
 {
@@ -15,6 +16,16 @@ let postBookAppointment = (data) =>
                 })
             } else
             {
+                await emailService.sendSimpleEmail({
+                    reciverEmail: data.email,
+                    patientName: 'Hoi dan it patient name',
+                    time: '8:00 - 9:00 Chủ nhật/2021',
+                    doctorName: 'BoyV',
+                    redirectLink: 'https://www.w3schools.com/'
+                })
+
+
+
                 //upsert patient
                 let user = await db.User.findOrCreate({
                     where: { email: data.email },
