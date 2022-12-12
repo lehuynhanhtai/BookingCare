c5529828dbea69de38027eda44e05a3df7b8cef7
Tai <lehuynhanhtai@gmail.com>
2022-12-12 10:07:57 +0700

    video 96

--- reactjs/src/services/userService.js
+++ reactjs/src/services/userService.js
@@ -101,6 +101,11 @@ const getDetailSpecialtyById = (data) =>
     return axios.get(`/api/get-detail-specialty-by-id?id=${data.id}&location=${data.location}`)
 }
 
+const createNewClinic = (data) =>
+{
+    return axios.post('/api/create-new-clinic', data)
+}
+
 export
 {
     handleLoginApi,
@@ -121,5 +126,6 @@ export
     postVerifyBookAppointment,
     createNewSpecialty,
     getAllSpecialty,
-    getDetailSpecialtyById
+    getDetailSpecialtyById,
+    createNewClinic
 }
